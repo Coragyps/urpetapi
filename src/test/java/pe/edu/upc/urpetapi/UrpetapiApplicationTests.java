376cc515b98d5872ad19c9be18fa76577a20e7de
Arturoqq <joserojasr23@gmail.com>
2024-04-03 13:39:16 -0500

    Autogenerado

---
+++ src/test/java/pe/edu/upc/urpetapi/UrpetapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.urpetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrpetapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
