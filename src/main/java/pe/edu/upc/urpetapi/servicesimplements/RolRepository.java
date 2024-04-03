440a99f34b4fa9b937b35217d9cd65dba1933da8
FABRIZIO HIDALGO <academyfabrizio7@gmail.com>
2024-04-03 18:34:10 -0500

    añadida repositorio rol

---
+++ src/main/java/pe/edu/upc/urpetapi/servicesimplements/RolRepository.java
@@ -0,0 +1,12 @@
+package pe.edu.upc.urpetapi.servicesimplements;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.urpetapi.repositories.iRolRepository;
+import pe.edu.upc.urpetapi.servicesinterfaces.iRolService;
+
+@Service
+public class RolRepository implements iRolService {
+    @Autowired
+    private iRolRepository rolR;
+}
