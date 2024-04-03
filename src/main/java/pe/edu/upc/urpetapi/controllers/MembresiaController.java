bd736ab707109f0ce42f1b3869f7f15cf00c9534
FABRIZIO HIDALGO <academyfabrizio7@gmail.com>
2024-04-03 18:32:38 -0500

    añadida controlador Membresia

---
+++ src/main/java/pe/edu/upc/urpetapi/controllers/MembresiaController.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.urpetapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.upc.urpetapi.servicesinterfaces.iMembresiaService;
+
+@RestController
+@RequestMapping("/Membresia")
+public class MembresiaController {
+    private iMembresiaService iMem;
+}
