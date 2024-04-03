37775c9c118800266cdfecc159a665d4cb117003
FABRIZIO HIDALGO <academyfabrizio7@gmail.com>
2024-04-03 18:33:53 -0500

    añadida Dto rol

---
+++ src/main/java/pe/edu/upc/urpetapi/dtos/RolDto.java
@@ -0,0 +1,22 @@
+package pe.edu.upc.urpetapi.dtos;
+
+public class RolDto {
+    private int RolId;
+    private String RolTipo;
+
+    public int getRolId() {
+        return RolId;
+    }
+
+    public void setRolId(int rolId) {
+        RolId = rolId;
+    }
+
+    public String getRolTipo() {
+        return RolTipo;
+    }
+
+    public void setRolTipo(String rolTipo) {
+        RolTipo = rolTipo;
+    }
+}
