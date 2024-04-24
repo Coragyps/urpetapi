7df891c84ff4dc859350a7427860220375efc904
MIRIAN <miriancontreraspaquita@gmail.com>
2024-04-24 18:13:24 -0500

    Implementación querys parte 1

--- src/main/java/pe/edu/upc/urpetapi/controllers/ReservaController.java
+++ src/main/java/pe/edu/upc/urpetapi/controllers/ReservaController.java
@@ -1,11 +1,22 @@
 package pe.edu.upc.urpetapi.controllers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pe.edu.upc.urpetapi.dtos.ReservaDto;
+import pe.edu.upc.urpetapi.entities.Reserva;
 import pe.edu.upc.urpetapi.servicesinterfaces.iReservaService;
 
 @RestController
 @RequestMapping("/reserva")
 public class ReservaController {
     private iReservaService resS;
+    @PostMapping("/solicitar")
+    public void SolicitarPaseo(@RequestBody ReservaDto reservaDto){
+        ModelMapper m=new ModelMapper();
+        Reserva rP=m.map(reservaDto,Reserva.class);
+        resS.SolicitarPaseo(rP);
+    }
 }
