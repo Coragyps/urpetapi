4824580d77ac83a143abefb21ce9f2fd8ac38e6d
MIRIAN <miriancontreraspaquita@gmail.com>
2024-04-24 18:13:01 -0500

    Implementación querys parte 1

--- src/main/java/pe/edu/upc/urpetapi/servicesinterfaces/iBoletaService.java
+++ src/main/java/pe/edu/upc/urpetapi/servicesinterfaces/iBoletaService.java
@@ -1,4 +1,12 @@
 package pe.edu.upc.urpetapi.servicesinterfaces;
 
+import org.springframework.data.repository.query.Param;
+import pe.edu.upc.urpetapi.dtos.BoletaDto;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface iBoletaService {
+
+    public List<BoletaDto> BoletasFechas(LocalDate fechaA, LocalDate fechaB);
 }
