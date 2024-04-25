90eedff4cd315adf9001dd95572b5149a5ee5f50
FABRIZIO HIDALGO <academyfabrizio7@gmail.com>
2024-04-24 20:55:38 -0500

    aqueries

--- src/main/java/pe/edu/upc/urpetapi/controllers/MembresiaController.java
+++ src/main/java/pe/edu/upc/urpetapi/controllers/MembresiaController.java
@@ -1,11 +1,23 @@
 package pe.edu.upc.urpetapi.controllers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pe.edu.upc.urpetapi.dtos.MembresiaDto;
+import pe.edu.upc.urpetapi.entities.Membresia;
 import pe.edu.upc.urpetapi.servicesinterfaces.iMembresiaService;
 
 @RestController
 @RequestMapping("/membresia")
 public class MembresiaController {
     private iMembresiaService memS;
+
+    @PostMapping("usuario/comprar")
+    public void CompraMembresia(@RequestBody MembresiaDto membresiaDto) {
+        ModelMapper m= new ModelMapper();
+        Membresia mT = m.map(membresiaDto, Membresia.class);
+        memS.ComprarMembresia(mT);
+    };
 }
